0d36c45743da7a0e922f8bdf21489e9b5b18deeb
dlguddn3214 <dlguddn3214@naver.com>
2018-12-27 17:52:00 +0900

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+const PORT = process.env.PORT
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -23,6 +23,6 @@ io.on('connection', function (socket) {
 io.emit('some event', { for: 'everyone' });
 
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+http.listen(PORT, function () {
+    console.log('listening on *:PORT');
 });
